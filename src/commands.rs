96d50810f5be7fcd7de03cc261802b7dd0adb82c
nedflux <me@nedflux.com>
2026-04-16 15:40:05 +0000

    Update commands.rs
    feat: implement 'aboutme' and 'help' commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,7 @@
 use crate::println;
 use crate::shell::MAX_COMMAND_ARGS_LEN;
 
-pub fn cowsays(args: [Option<&str>; MAX_COMMAND_ARGS_LEN]) {
+pub fn cowsay(args: [Option<&str>; MAX_COMMAND_ARGS_LEN]) {
     println!("
    ({})
      \\   ^__^
@@ -13,4 +13,34 @@ pub fn cowsays(args: [Option<&str>; MAX_COMMAND_ARGS_LEN]) {
 }
 pub fn whoami() {
     println!("Hey, my name is Alexey");
+}
+pub fn help() {
+    println!("
+x86-64 rust kernel.
+commands:
+
+    cowsay [ARGS]: Print a cow and exit.
+
+    help: Print this help and exit.
+
+    whoami: Print my name.
+
+    aboutme: Print some information about me.
+
+    clear: clear the screen.
+        ")
+}
+
+pub fn aboutme() {
+    println!("
+Dev: Alexey K.
+
+What is this?: Written in rust kernel for x86_64 arch.
+
+Why did I choose this architecture?:\n The only argument is that there\n is more training material for this architecture,\n otherwise I would prefer RISC-V,\n which I will implement in the next project
+
+My goals: Understand the Rust operating system development base.
+
+My real OS: Gentoo linux hardened x86_64.
+        ")
 }
